645c383d356df4c00f95fdb835244c16178a13a4
Anurag Dhadse <anudhadse66@gmail.com>
2022-01-20 17:00:55 +0530

    moved functions

--- valid_identifier.go
+++ valid_identifier.go
@@ -27,14 +27,3 @@ func IsValidIdentifier(s string) bool {
 	}
 	return true
 }
-
-func IsValidKeyword(s string) bool {
-	if s == TOK_CONTINUE ||
-		s == TOK_IF ||
-		s == TOK_ELSE ||
-		s == TOK_BREAK ||
-		s == TOK_FOR {
-		return true
-	}
-	return false
-}
--- valid_keyword.go
+++ valid_keyword.go
@@ -1,1 +1,12 @@
 package CompilerDesign
+
+func IsValidKeyword(s string) bool {
+	if s == TOK_CONTINUE ||
+		s == TOK_IF ||
+		s == TOK_ELSE ||
+		s == TOK_BREAK ||
+		s == TOK_FOR {
+		return true
+	}
+	return false
+}
